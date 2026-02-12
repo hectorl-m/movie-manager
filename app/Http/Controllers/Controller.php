1993ae2aa79660f1aed12ffbcb7c86e0cda99d5c
Hector <hectorlm203@gmail.com>
2026-02-12 20:12:10 +0100

    primer commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    // auth()->user()->can('admin');
+}
---
+++ app/Models/Movie.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = [
+        'tmdb_id',
+        'title',
+        'release_date',
+        'overview',
+        'runtime',
+        'poster_path',
+    ];
+}
